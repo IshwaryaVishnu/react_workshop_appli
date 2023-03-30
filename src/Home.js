8004fdf27ca696e06984f693311341c0cc81a4d0
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-03-30 16:51:40 +0200

    Part3

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to our homepage!</h1>
+      <p>Here you can find information about our products and services.</p>
+    </div>
+  );
+};
+
+export default Home;
